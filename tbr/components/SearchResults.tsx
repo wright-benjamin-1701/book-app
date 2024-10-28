04363ff05a26552fa3ed79c2dd92e1ded1308908
jv24601 <108680838+wright-benjamin-1701@users.noreply.github.com>
2024-10-27 20:39:46 -0500

    add pagination

--- tbr/components/SearchResults.tsx
+++ tbr/components/SearchResults.tsx
@@ -1,23 +1,47 @@
-import React from 'react';
-import { FlatList, StyleSheet } from 'react-native';
+import React, { useCallback, useState } from 'react';
+import { Button, FlatList, StyleSheet, View } from 'react-native';
 import BookResult from '@/components/BookResult'; // Assuming you have a BookResult component defined somewhere
 import { Book } from '@/types';
 
+
 interface Props {
     books?: Book[];
-
 }
 export default function SearchResults(props:Props) {
   const { books } = props;
 
-  const slicedBooks = books?.slice(0, 10);
+  const resultsPerPage = 5;
+
+  const slices = [];
+
+  for (let i = 0; i < Math.ceil((books?.length || 0) / resultsPerPage); i++) {
+    slices.push(books?.slice(i * resultsPerPage, (i + 1) * resultsPerPage));
+  }
+
+  const [index, setIndex] = useState(0);
+
+  const onIndexChanged = useCallback((index:number)=> {setIndex(index);},[setIndex]);
+
   return (
-    <FlatList
-      data={slicedBooks}
-      renderItem={({ item }) => <BookResult book={item} />}
-      keyExtractor={(item) => item.isbn}
-      style={styles.container}
-    />
+    <>
+
+        <View>
+          <FlatList
+            data={slices[index]}
+            renderItem={({ item }) => <BookResult book={item} />}
+            keyExtractor={(item) => item.isbn}
+            style={styles.container}
+          />
+        </View>
+        <View style={{ flexDirection: 'row', alignSelf:'center'}} >
+                  {slices.map((_slicedBooks,_index) => (
+
+            <Button title={(_index + 1).toString()} onPress={()=>(setIndex(_index))}
+              disabled={index === _index} key={_index} />
+
+        ))}
+        </View>
+    </>
   );
 }
 
